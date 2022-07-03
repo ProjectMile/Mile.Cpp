5b28d6f4d72a23a68eaeff6bacc6e4b655dd0009
MouriNaruto <Mouri_Naruto@Outlook.com>
2022-07-03 18:48:26 +0800

    Add Mile.Windows.DpiHelpers.

---
+++ Mile.Library.Windows/Mile.Windows.DpiHelpers.cpp
@@ -0,0 +1,11 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      Mile.Windows.DpiHelpers.cpp
+ * PURPOSE:   Implementation for Windows DPI Helpers
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+#include "Mile.Windows.DpiHelpers.h"
---
+++ Mile.Library.Windows/Mile.Windows.DpiHelpers.h
@@ -0,0 +1,18 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      Mile.Windows.DpiHelpers.h
+ * PURPOSE:   Definition for Windows DPI Helpers
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+#pragma once
+
+#ifndef MILE_WINDOWS_DPIHELPERS
+#define MILE_WINDOWS_DPIHELPERS
+
+#include <Windows.h>
+
+#endif // !MILE_WINDOWS_DPIHELPERS
