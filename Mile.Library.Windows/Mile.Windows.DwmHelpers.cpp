d3c8c5d0790bbe3f50c668dbe576b23db66316ff
MouriNaruto <Mouri_Naruto@Outlook.com>
2022-07-07 19:26:17 +0800

    Add Mile.Windows.DwmHelpers.

---
+++ Mile.Library.Windows/Mile.Windows.DwmHelpers.cpp
@@ -0,0 +1,12 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      Mile.Windows.DwmHelpers.cpp
+ * PURPOSE:   Implementation for Windows DWM Helpers
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+#include "Mile.Windows.DwmHelpers.h"
+
---
+++ Mile.Library.Windows/Mile.Windows.DwmHelpers.h
@@ -0,0 +1,18 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      Mile.Windows.DwmHelpers.h
+ * PURPOSE:   Definition for Windows DWM Helpers
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+#pragma once
+
+#ifndef MILE_WINDOWS_DWMHELPERS
+#define MILE_WINDOWS_DWMHELPERS
+
+#include <Windows.h>
+
+#endif // !MILE_WINDOWS_DWMHELPERS
