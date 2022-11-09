998b36adc463a0d5b0bf893d6dd68046d95ddb34
MouriNaruto <Mouri_Naruto@Outlook.com>
2022-11-10 04:34:12 +0800

    Remove MileSetUseImmersiveDarkModeAttribute due to Mile.Windows.Helpers implemented it.

--- Mile.Library.Windows/Mile.Windows.DwmHelpers.cpp
+++ Mile.Library.Windows/Mile.Windows.DwmHelpers.cpp
@@ -13,51 +13,6 @@
 #include <dwmapi.h>
 #pragma comment(lib, "dwmapi.lib")
 
-namespace
-{
-    static bool IsWindows10Version20H1OrLater()
-    {
-        static bool CachedResult = ([]() -> bool
-        {
-            OSVERSIONINFOEXW OSVersionInfoEx = { 0 };
-            OSVersionInfoEx.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEXW);
-            OSVersionInfoEx.dwMajorVersion = 10;
-            OSVersionInfoEx.dwMinorVersion = 0;
-            OSVersionInfoEx.dwBuildNumber = 19041;
-            return ::VerifyVersionInfoW(
-                &OSVersionInfoEx,
-                VER_BUILDNUMBER,
-                ::VerSetConditionMask(
-                    ::VerSetConditionMask(
-                        ::VerSetConditionMask(
-                            0,
-                            VER_MAJORVERSION,
-                            VER_GREATER_EQUAL),
-                        VER_MINORVERSION,
-                        VER_GREATER_EQUAL),
-                    VER_BUILDNUMBER,
-                    VER_GREATER_EQUAL));
-        }());
-
-        return CachedResult;
-    }
-}
-
-EXTERN_C HRESULT WINAPI MileSetUseImmersiveDarkModeAttribute(
-    _In_ HWND WindowHandle,
-    _In_ BOOL Value)
-{
-    const DWORD DwmWindowUseImmersiveDarkModeBefore20H1Attribute = 19;
-    const DWORD DwmWindowUseImmersiveDarkModeAttribute = 20;
-    return ::DwmSetWindowAttribute(
-        WindowHandle,
-        (::IsWindows10Version20H1OrLater()
-            ? DwmWindowUseImmersiveDarkModeAttribute
-            : DwmWindowUseImmersiveDarkModeBefore20H1Attribute),
-        &Value,
-        sizeof(BOOL));
-}
-
 EXTERN_C HRESULT WINAPI MileSetCaptionColorAttribute(
     _In_ HWND WindowHandle,
     _In_ COLORREF Value)
--- Mile.Library.Windows/Mile.Windows.DwmHelpers.h
+++ Mile.Library.Windows/Mile.Windows.DwmHelpers.h
@@ -16,20 +16,6 @@
 #include <Windows.h>
 
 /**
- * @brief Allows the window frame for this window to be drawn in dark mode
- *        colors when the dark mode system setting is enabled.
- * @param WindowHandle The handle to the window for which the attribute value
- *                     is to be set.
- * @param Value TRUE to honor dark mode for the window, FALSE to always use
- *              light mode.
- * @return If the function succeeds, it returns S_OK. Otherwise, it returns an
- *         HRESULT error code.
-*/
-EXTERN_C HRESULT WINAPI MileSetUseImmersiveDarkModeAttribute(
-    _In_ HWND WindowHandle,
-    _In_ BOOL Value);
-
-/**
  * @brief Specifies the color of the caption.
  * @param WindowHandle The handle to the window for which the attribute value
  *                     is to be set.
